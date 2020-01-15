709783e7a27ee80abf00c57312ae8e77cd94b831
sqvall <masster1987@inbox.ru>
2020-01-15 23:17:04 +0300

    * test typescript

---
+++ test2.js
@@ -0,0 +1,3 @@
+foo = 3;
+console.log("Half the number of widgets is " + (foo / 2));
+//# sourceMappingURL=test2.js.map
---
+++ test2.ts
@@ -0,0 +1,8 @@
+declare var foo: number;
+// foo = 3;
+
+// Intend exception error!
+foo = "23";
+
+console.log("Half the number of widgets is " + (foo / 2));
+
