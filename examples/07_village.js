81f980074dd9a74fc91f20889ed198650de60757
sqvall <masster1987@inbox.ru>
2020-02-18 23:15:44 +0300

    *

--- examples/07_village.js
+++ examples/07_village.js
@@ -55,6 +55,21 @@ class VillageState {
       return new VillageState(destination, parcels);
     }
   }
+
+  static random () {
+    // Generate random parcel list
+    const parcelCount = 5;
+    const parcels = [];
+    for (let i = 0; i < parcelCount; i++) {
+      const address = randomPick(Object.keys(roadGraph));
+      let place;
+      do {
+        place = randomPick(Object.keys(roadGraph));
+      } while (place === address);
+      parcels.push({ place, address });
+    }
+    return new VillageState('Почта', parcels);
+  }
 }
 
 const first = new VillageState('Почта', [{ place: 'Почта', address: 'Дом Алисы' }]);
@@ -64,3 +79,46 @@ console.log(first.place);
 console.log(first.parcels);
 console.log(next.place);
 console.log(next.parcels);
+
+const runRobot = (state, robot, memory) => {
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.length === 0) {
+      console.log(`Выполнено за ${turn} ходов`);
+      break;
+    }
+    const action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Переход в направлении ${action.direction}`);
+  }
+};
+
+const randomPick = (array) => {
+  // random pick element from array
+  const choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+};
+
+const randomRobot = (state) => {
+  // random robot
+  // console.log(state);
+  return { direction: randomPick(roadGraph[state.place]) };
+};
+
+// runRobot(VillageState.random(), randomRobot); // client code
+
+const mailRoute = [
+  // from simple example: create manual route
+  'Дом Алисы', 'Склад', 'Дом Алисы', 'Дом Боба', 'Ратуша', 'Дом Дарии', 'Дом Эрни', // careful book error Сарай -> Склад
+  'Дом Греты', 'Магазин', 'Дом Греты', 'Ферма', 'Рынок', 'Почта'
+];
+
+const routeRobot = (state, memory) => {
+  // simple route robot with memory
+  if (memory.length === 0) {
+    memory = mailRoute;
+  }
+  return { direction: memory[0], memory: memory.slice(1) };
+};
+
+runRobot(VillageState.random(), routeRobot, []); // client code
